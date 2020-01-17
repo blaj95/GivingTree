1aa59ebac8c28e6148906ac79d8668c02ed8b943
blaj95 <32546217+blaj95@users.noreply.github.com>
2020-01-17 13:48:10 -0500

    Basic app communication in place

---
+++ Assets/Scripts/Networking/Ping.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon;
+using Photon.Pun;
+using TMPro;
+using UnityEngine;
+
+public class Ping : MonoBehaviourPun
+{
+    public TMP_Text pingText;
+    
+    public void SendPing()
+    {
+       photonView.RPC("PingRPC",RpcTarget.Others,photonView.ViewID.ToString());
+    }
+
+    [PunRPC]
+    public void PingRPC(string id)
+    {
+        pingText.text += "Message Received from " + id;
+    }
+}
