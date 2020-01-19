3e7cb6876ec8b2290b7966dcc26fb82aa5ee787a
blaj95 <32546217+blaj95@users.noreply.github.com>
2020-01-19 12:00:23 -0500

    First screen added

---
+++ Assets/Scripts/ChooseCause.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class ChooseCause : MonoBehaviour
+{
+    public GameObject arCamera, scanTip;
+    public ARPlaneManager planeManager;
+    public TreePlacement treePlacement;
+    public GameObject tracker;
+    
+    public void OnCauseChoosen()
+    {
+        planeManager.enabled = true;
+        scanTip.SetActive(true);
+        tracker.SetActive(true);
+        treePlacement.enabled = true;
+        gameObject.SetActive(false);
+    }
+}
